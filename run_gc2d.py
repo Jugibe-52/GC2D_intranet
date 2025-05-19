1a01f1e1dc30c74b385dd73a95a3c70c7cb5f5a6
Cristel Chandre <cchandre@gmail.com>
2025-05-19 13:14:23 +0200

    Added symplectic scheme with B-spline interpolation

---
+++ run_gc2d.py
@@ -0,0 +1,34 @@
+###################################################################################################
+##                      Parameters: https://github.com/cchandre/GC2D_intranet                    ##
+###################################################################################################
+
+import numpy as xp
+import matplotlib.pyplot as plt
+from gc2d_classes import mock_potential, GC2Ds
+
+# potential
+M = 25
+A = 0.6
+Nx, Ny = 64, 64
+potential = mock_potential(A, M, Nx, Ny)
+
+# parameters
+rho, eta = 0, 0
+Ntraj = 40
+t_max = 1500
+traj_type = "gc" # 'gc' (guiding centers) or 'fo' (full orbits) 
+
+traj = {"type": traj_type, "rho": rho, "eta": eta}
+hs = GC2Ds(potential, traj)
+z0 = hs.initial_conditions(Ntraj, type="random")
+
+t_eval = 2 * xp.pi * xp.arange(t_max)
+sol = hs.integrate(z0, t_eval, timestep=1e-1)
+
+# Plot of the Poincaré section
+x, y = xp.split(sol.y, 2)
+plt.plot(x % potential.period, y % potential.period, '.', color='blue')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
