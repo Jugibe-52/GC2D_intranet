1fb76e739907ec99b6e724613594e5b49812bd53
juan <juangilbescos52@gmail.com>
2026-07-01 14:09:32 +0200

    Remove legacy symplectic dict adapter

--- src/gc2d_core/gc2d_symp.py
+++ src/gc2d_core/gc2d_symp.py
@@ -28,6 +28,7 @@
 import numpy as xp
 import os
 import logging
+import argparse
 from typing import Any, Literal
 os.environ.setdefault("MPLCONFIGDIR", ".matplotlib")
 from scipy.special import jv
@@ -35,27 +36,48 @@ import multiprocess
 from datetime import datetime
 from scipy.io import savemat
 from .gc2d_symp_modules import run_method
-from .config import load_gc2dt_config
+from .config import DEFAULT_CONFIG_VERSION, load_gc2dt_config
 from .logging_config import configure_logging
 from pyhamsys import OdeSolution, HamSys
 
 logger = logging.getLogger(__name__)
 
+def parse_args() -> argparse.Namespace:
+	parser = argparse.ArgumentParser(description="Run the legacy GC2Dt symplectic workflow from JSON.")
+	parser.add_argument("--config", help="Path to the JSON configuration file.")
+	parser.add_argument("--config-group", default="assay", choices=("test", "assay"), help="Configuration group under conf/.")
+	parser.add_argument("--config-version", default=DEFAULT_CONFIG_VERSION, help="Configuration folder version under conf/<group>/, e.g. v_1.")
+	parser.add_argument("--version", default="symplectic_grid", help="Profile inside the JSON file.")
+	return parser.parse_args()
+
+
 def main() -> None:
+	args = parse_args()
 	configure_logging()
-	config = load_gc2dt_config(config_group="assay", config_version="v_1", version="symplectic_grid")
-	dict_list = config.cases()
+	config = load_gc2dt_config(
+		args.config,
+		version=args.version,
+		config_group=args.config_group,
+		config_version=args.config_version,
+	)
+	params_list = config.cases()
 	parallelization = config.parallelization
 	if parallelization == 'all':
 		num_cores = multiprocess.cpu_count()
 	else:
 		num_cores = min(multiprocess.cpu_count(), int(parallelization))
-	logger.info("Prepared %d case(s); parallelization=%s; workers=%d", len(dict_list), parallelization, num_cores)
+	logger.info(
+		"Prepared %d case(s) from config version=%s; parallelization=%s; workers=%d",
+		len(params_list),
+		config.version,
+		parallelization,
+		num_cores,
+	)
 	if num_cores >= 2:
 		pool = multiprocess.Pool(num_cores)
-		pool.map(lambda dict_: run_method(GC2Dt(dict_)), dict_list)
+		pool.map(lambda dict_: run_method(GC2Dt(dict_)), params_list)
 	else:
-		for dict_ in dict_list:
+		for dict_ in params_list:
 			run_method(GC2Dt(dict_))
 	logger.info("All cases finished")
 
--- src/gc2d_core/gc2d_symp_dict.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility adapter for the legacy symplectic grid configuration."""
-
-from .config import load_gc2dt_config
-
-_config = load_gc2dt_config(config_group="assay", config_version="v_1", version="symplectic_grid")
-
-dict_list = _config.cases()
-Parallelization = _config.parallelization
